1778ad503d9adb61363bf92f927fd9896db5f9a4
tongoonamujera <tongoonamujera@gmail.com>
2021-11-11 15:34:50 +0200

    Add more changes

--- game.rb
+++ game.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require_relative './game_board.rb'
 
 class GameScreen < Gosu::Window
   def initialize(width= 520, height = 320, fullscreen = false)
@@ -36,7 +37,13 @@ class GameScreen < Gosu::Window
   end
 
   def screen
-    "|0|1|2|\n"
+    a = GameBoard.new
+    "+---+---+---+"
+    "| #{a.avail_choices[0]} | #{a.avail_choices[1]} | #{a.avail_choices[2]} |"
+    "+---+---+---+"
+    "| #{a.avail_choices[3]} | #{a.avail_choices[4]} | #{a.avail_choices[5]} |"
+    "+---+---+---+"
+    "| #{a.avail_choices[6]} | #{a.avail_choices[7]} | #{a.avail_choices[8]} | \n  ~   ~   ~  "
   end
 
   def time
---
+++ game_board.rb
@@ -0,0 +1,9 @@
+class GameBoard
+  def initialize
+    @avail_choices = Array(1..9)
+  end
+
+  def avail_choices
+    @avail_choices
+  end
+end
